950bd9fab3d8e57c93c86c2ff8295fe123e08998
Murillo Sergio <murillosergionsousa@outlook.com>
2025-05-10 15:57:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let temas = document.getElementById('temas')
+let alterar_tema = document.getElementById('alterar_tema')
 let body = document.querySelector('body')
 
-temas.addEventListener('click', ()=>{
-    temas.classList.toggle('dark')
+alterar_tema.addEventListener('click', ()=>{
+    alterar_tema.classList.toggle('dark')
     body.classList.toggle('dark')
 })
 
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('botao-mensagem').addEventListener('click', mostrarMensagem);
+});
+
 function mostrarMensagem() {
-      const mensagem = "Bem-vindo a minha página!";
-      document.getElementById("mensagem").textContent = mensagem;
-    }
+  document.getElementById('mensagem').innerText = "Seja bem-vindo à página!";
+}
